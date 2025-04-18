a75ca5172c108e8c403856daf66f66cffe542c1b
saikatsingh <mailmetosaikat676@gmail.com>
2025-04-18 13:56:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,8 +21,8 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6400371201').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Leazy_Boy") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002187892688'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/allmovierequesthub')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002434067416').split()]
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Creazy_Movie_Surch_Group')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002287841044 -1002385499857 -1002451264196 -1002457606395 -1002283169339 -1002308876940 -1002509542265').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mailmetosaikat676:saikat9735@cluster0.2esif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
