90e2fb85fd564fbef307079e915537ee8935cf64
mv vaibhav <154995597+mvvaibhav@users.noreply.github.com>
2025-04-18 20:47:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -92,4 +92,4 @@ if __name__ == '__main__':
     try:
         loop.run_until_complete(Jisshu_start())
     except KeyboardInterrupt:
-        logging.info('Service Stopped Bye 👋')
+        logging.info('💼 sᴇʀᴠɪᴄᴇ sᴛᴏᴘᴘᴇᴅ ᴛᴀᴛᴀ 🚫')
