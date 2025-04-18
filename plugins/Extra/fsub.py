9c36108e32bb103054a85538ca13d730766e011a
mv vaibhav <154995597+mvvaibhav@users.noreply.github.com>
2025-04-18 22:29:58 +0530

    Update fsub.py

--- plugins/Extra/fsub.py
+++ plugins/Extra/fsub.py
@@ -11,54 +11,54 @@ logger = logging.getLogger(__name__)
 async def force_subscribe(client, message):
     m = await message.reply_text("Wait im checking...")
     if not message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        return await m.edit("This command is only for groups!")
+        return await m.edit("**ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ɪs ᴏɴʟʏ ғᴏʀ ɢʀᴏᴜᴘs 💢**")
     if not await is_check_admin(client, message.chat.id, message.from_user.id):
-        return await m.edit("Only group admins can use this command!")
+        return await m.edit("**ᴏɴʟʏ ɢʀᴏᴜᴘ ᴀᴅᴍɪɴs ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ! 💼**")
     try: 
         toFsub = message.command[1]
     except IndexError:
-        return await m.edit("Usage: /fsub CHAT_ID")
+        return await m.edit("**ᴜsᴀɢᴇ - /fsub ᴄʜᴀᴛ_ɪᴅ**")
     if not toFsub.startswith("-100"):
         toFsub = '-100'+toFsub
     if not toFsub[1:].isdigit() or len(toFsub) != 14:
-        return await m.edit("CHAT_ID isn't valid!")
+        return await m.edit("**ᴄʜᴀᴛ_ɪᴅ ɪsɴ'ᴛ ᴠᴀʟɪᴅ! 🚫**")
     toFsub = int(toFsub)
     if toFsub == message.chat.id:
-        return await m.edit("It seems like you're attempting to enable force subscription for this chat ID. Please use a different chat ID !")
+        return await m.edit("**ᴘʟᴇᴀsᴇ ᴜsᴇ ᴀ ᴅɪғғᴇʀᴇɴᴛ ᴄʜᴀᴛ ɪᴅ! ✴️**")
     if not await is_check_admin(client, toFsub, client.me.id):
-        return await m.edit("I need to be an admin in the given chat to perform this action!\nMake me admin in your Target chat and try again.")
+        return await m.edit("<b>ɪ ɴᴇᴇᴅ ᴛᴏ ʙᴇ ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ɢɪᴠᴇɴ ᴄʜᴀᴛ ᴛᴏ ᴘᴇʀғᴏʀᴍ ᴛʜɪs ᴀᴄᴛɪᴏɴ!\n<u>ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ ɪɴ ʏᴏᴜʀ ᴛᴀʀɢᴇᴛ ᴄʜᴀᴛ ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ.</u></b>")
     try:
         await db.setFsub(grpID=message.chat.id, fsubID=toFsub)
-        return await m.edit(f"Successfully added force subscribe to {toFsub} in {message.chat.title}")
+        return await m.edit(f"**✅ sᴜᴄᴄᴇssғᴜʟʟʏ ᴀᴅᴅᴇᴅ ғᴏʀᴄᴇ sᴜʙsᴄʀɪʙᴇ ᴛᴏ {toFsub} {message.chat.title}**")
     except Exception as e:
         logger.exception(e)
-        return await m.edit(f"Something went wrong ! Try again later or report in Support Group @Jisshu_support")
+        return await m.edit(f"**sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ, ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ ᴏʀ ʀᴇᴘᴏʀᴛ ɪɴ @I_M_STARBOY **")
 
 @Client.on_message(filters.command("del_fsub"))
 async def del_force_subscribe(client, message):
-    m = await message.reply_text("Wait im checking...")
+    m = await message.reply_text("**⏳ ᴡᴀɪᴛ ɪᴍ ᴄʜᴇᴄᴋɪɴɢ...**")
     if not message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        return await m.edit("This command is only for groups!")
+        return await m.edit("**ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ɪs ᴏɴʟʏ ғᴏʀ ɢʀᴏᴜᴘs 💢**")
     if not await is_check_admin(client, message.chat.id, message.from_user.id):
-        return await m.edit("Only group admins can use this command!")
+        return await m.edit("")
     ifDeleted =await db.delFsub(message.chat.id)
     if ifDeleted:
-        return await m.edit(f"Successfully removed force subscribe for - {message.chat.title}\nTo add again use <code>/fsub YOUR_FSUB_CHAT_ID</code>")
+        return await m.edit(f"**🚫 sᴜᴄᴄᴇssғᴜʟʟʏ ʀᴇᴍᴏᴠᴇᴅ ғᴏʀᴄᴇ sᴜʙsᴄʀɪʙᴇ ғᴏʀ - {message.chat.title}\nᴛᴏ ᴀᴅᴅ ᴀɢᴀɪɴ ᴜsᴇ <code>/fsub YOUR_FSUB_CHAT_ID</code>**")
     else:
-        return await m.edit(f"Force subscribe not found in {message.chat.title}")
+        return await m.edit(f"**ғᴏʀᴄᴇ sᴜʙsᴄʀɪʙᴇ ɴᴏᴛ ғᴏᴜɴᴅ ɪɴ ❌ {message.chat.title}**")
 
 @Client.on_message(filters.command("show_fsub"))
 async def show_fsub(client, message):
-    m = await message.reply_text("Wait im checking...")
+    m = await message.reply_text("⏳ ᴡᴀɪᴛ ɪᴍ ᴄʜᴇᴄᴋɪɴɢ...")
     if not message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        return await m.edit("This command is only for groups!")
+        return await m.edit("**ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ɪs ᴏɴʟʏ ғᴏʀ ɢʀᴏᴜᴘs 💢**")
     # check if commad is given by admin or not
     if not await is_check_admin(client, message.chat.id, message.from_user.id):
-        return await m.edit("Only group admins can use this command!")
+        return await m.edit("**ᴏɴʟʏ ɢʀᴏᴜᴘ ᴀᴅᴍɪɴs ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ! 💼**")
     fsub = await db.getFsub(message.chat.id)
     if fsub:
         #now gen a invite link
         invite_link = await client.export_chat_invite_link(fsub)
-        await m.edit(f"Force subscribe is set to {fsub}\n<a href={invite_link}>Channel Link Link</a>" ,disable_web_page_preview=True)
+        await m.edit(f"» ғᴏʀᴄᴇ sᴜʙsᴄʀɪʙᴇ ɪs sᴇᴛ ᴛᴏ {fsub}\n<a href={invite_link}» ᴄʜᴀɴɴᴇʟ ʟɪɴᴋ </a>" ,disable_web_page_preview=True)
     else:
-        await m.edit(f"Force subscribe is not set in {message.chat.title}")
+        await m.edit(f"**ғᴏʀᴄᴇ sᴜʙsᴄʀɪʙᴇ ɪs ɴᴏᴛ sᴇᴛ ɪɴ {message.chat.title} ❌ **")
