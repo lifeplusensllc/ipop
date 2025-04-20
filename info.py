4be82067f267afc2fdfba55628c77c444136ec9e
mv vaibhav <154995597+mvvaibhav@users.noreply.github.com>
2025-04-20 15:51:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -68,7 +68,7 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-START_IMG = (environ.get('START_IMG', 'https://envs.sh/YoH.jpg')).split()
+START_IMG = (environ.get('START_IMG', 'https://ping.skimpray.site/img/file_177.jpg')).split()
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/5b4531bb88bad855e4680.jpg')).split()
