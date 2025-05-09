51ce4751ed356626dc307ab6c1bcbbb4a6baa0a2
lifeplusensllc <157949830+lifeplusensllc@users.noreply.github.com>
2025-05-09 21:57:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,20 +13,20 @@ def is_enabled(value, default):
 #---------------------------------------------------------------
 #---------------------------------------------------------------         ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '27100881'))
-API_HASH = environ.get('API_HASH', 'ba917e1a377fa4ea750d9253bcaf9940')
-BOT_TOKEN = environ.get('BOT_TOKEN', '8046581218:AAEKUror1d_3ddCrDb3VtalUsOAgG2rKeeQ')
+API_ID = int(environ.get('API_ID', '271881'))
+API_HASH = environ.get('API_HASH', 'ba9a377fa4ea750d9253bcaf9940')
+BOT_TOKEN = environ.get('BOT_TOKEN', '804218:AAEKUror1d_3ddCrDb3VtalUsOAgG2rKeeQ')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1936430521').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6430521').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/i_m_starboy") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001946959926'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-10019926'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/hd_request')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001940024711').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-10024711').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://iPapkornBot:iPapkornBot@ipapkornbot.xt5638x.mongodb.net/?retryWrites=true&w=majority&appName=iPapkornBot")
-DATABASE_NAME = environ.get('DATABASE_NAME', "iPapkornBot")
+DATABASE_URI = environ.get('DATABASE_URI', "mongPapkornBot")
+DATABASE_NAME = environ.get('DATABASE_NAME', "ot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
@@ -35,7 +35,7 @@ LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '0'))
 BIN_CHANNEL = int(environ.get('BIN_CHANNEL','0'))
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','0'))
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '0'))
-auth_channel = environ.get('AUTH_CHANNEL', '-1001596571772')
+auth_channel = environ.get('AUTH_CHANNEL', '-772')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
 request_channel = environ.get('REQUEST_CHANNEL', '0')
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '0'))
@@ -46,11 +46,11 @@ IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/How_To_Download_28")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
-SHORTENER_API = environ.get("SHORTENER_API", "be8b066888466b4de6ed6b96744c6e9746a5796b")
+SHORTENER_API = environ.get("SHORTENER_API", "be8b06de6ed6b96744c6e9746a5796b")
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'mdiskshortner.link')
-SHORTENER_API2 = environ.get("SHORTENER_API2", "be8b066888466b4de6ed6b96744c6e9746a5796b")
+SHORTENER_API2 = environ.get("SHORTENER_API2", "be8b04de6ed6b96744c6e9746a5796b")
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'mdiskshortner.link')
-SHORTENER_API3 = environ.get("SHORTENER_API3", "be8b066888466b4de6ed6b96744c6e9746a5796b")
+SHORTENER_API3 = environ.get("SHORTENER_API3", "be8b4de6ed6b96744c6e9746a5796b")
 SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'mdiskshortner.link')
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "300"))
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "14400"))
